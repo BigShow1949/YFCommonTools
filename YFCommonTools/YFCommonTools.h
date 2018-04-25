eecc40c09294d2f38aa8455fd00b1a9be687b0ea
BigShow1949 <1029883589@qq.com>
2018-04-25 15:46:26 +0800

    新建.h文件

---
+++ YFCommonTools/YFCommonTools.h
@@ -0,0 +1,15 @@
+//
+//  YFCommonTools.h
+//  YFToolsDemo
+//
+//  Created by apple on 2018/4/25.
+//  Copyright © 2018年 BigShow. All rights reserved.
+//
+
+#ifndef YFCommonTools_h
+#define YFCommonTools_h
+
+#import "SafeObject.h"
+#import "JZLocationConverter"
+
+#endif /* YFCommonTools_h */
